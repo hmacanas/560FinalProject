272307d6be868c6f23247c28d36cfb0388a06f31
MichaelCaudill <mica1563@colorado.edu>
2019-05-25 18:48:44 -0700

    new main

--- functions/DGVSCMG.m
+++ functions/DGVSCMG.m
@@ -1,15 +1,23 @@
-function [xdot] = DGVSCMG(t,x,J,Iws)
-
+function [xdot] = DGVSCMG(t,x,J,Iws,kp,kd)
 % DECOMPOSE STATE VECTOR
+mu = 7.9E12;
+a = 7359.42;
+inc = deg2rad(53);
+n = 0.001;
+
 % Spacecraft
 we = [x(1);x(2);x(3)]; % [rad/s] angular velocity error
 se = [x(4);x(5);x(6)]; % MRP Error
-
+state = [we;se];
 % CMG
 Omega = x(7); % [rad/s] wheel speed
 d_i   = x(8); % [rad] inner gimbal angle
 d_o   = x(9); % [rad] outer gimbal angle
 
+
+%% CALCULATIONS
+% Calculate Uprime
+uPrime = -[kd zeros(3); zeros(3) kp]*state;
 % Calculate ue
 ue = [Omega-300; d_i; 0];
 
@@ -25,7 +33,7 @@ A = [A zeros(3); 0.25*H zeros(3)];
 % B Matrices
 [B1Bar] = getB1Bar(J, Iws);
 [B2Bar] = getB2Bar(J, mu, a);
-Be = [Bbar1;zeros(3)];
+Be = [B1Bar;zeros(3)];
 
 % E Matrix
 
@@ -34,11 +42,16 @@ Be = [Bbar1;zeros(3)];
 [N2] = getN2(t, inc, n);
 
 % Calculate N# matrices
+k1 = 10;
+k1Prime = 0.001;
+k2 = 0.01;
 [lambda1] = getLambda1(k1, k1Prime, N1);
-[N1Sharp] = getN1Sharp(N1, lambda1, I);
-[N2Sharp] = getN2Sharp(N2, k2, I);
+[N1Sharp] = getN1Sharp(N1, lambda1, eye(3));
+[N2Sharp] = getN2Sharp(N2, k2, eye(3));
 
 % Calculate MTQ and UDGV
+b1 = 1;
+b2 = 0.1;
 WBar = [b1 0 0; 0 b2 0; 0 0 0];
 [MTQ] = getMTQ(N2Sharp, B2Bar, B1Bar, N1, WBar, ue);
 [UDGV] = getUDGV(N1Sharp, uPrime, WBar, ue, B1Bar, B2Bar, N2, MTQ);
--- functions/getN1Sharp.m
+++ functions/getN1Sharp.m
@@ -1,4 +1,4 @@
 function [N1Sharp] = getN1Sharp(N1, lambda1, I)
-    N1Sharp = N1'*inv(N1*N1'+lambda1*I));
+    N1Sharp = N1'*inv(N1*N1'+lambda1*I);
 end
 
